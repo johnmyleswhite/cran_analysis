3395b7f15fc613250b14530e7327153348874c36
Drew Conway <agconway@gmail.com>
2010-08-28 02:35:27 +0800

    Add multiple graph measures, and file exports

--- r_dependency_net.py
+++ r_dependency_net.py
@@ -4,6 +4,10 @@
 r_dependency_net.py
 
 Purpose:  Analyze the dependency network of R libraries
+            and output as GraphML file with centrality 
+            node attributes.
+            
+            Also do some basic visualization.
 
 Author:   Drew Conway
 Email:    drew.conway@nyu.edu
@@ -18,25 +22,49 @@ import sys
 import os
 import networkx as nx
 import pygraphviz as pgv
-import matplotli.pylab as plt
+import matplotlib.pylab as plt
 
+def multiple_measures(g,function_list):
+    """Applies a list of NX centrality measures to a graph object,
+    then adds data to each node in g as attibutes."""
+    try:
+        measures=map(lambda f: f(g), function_list)
+    except NetworkXError:
+        print("An error occurred trying to apply a function to the graph")
+    measures_dict=dict(zip(function_list,measures))
+    # Add measures of node v to graph g as NX node attributes
+    for v in g.nodes_iter():
+        v_measures=dict([(str(a).split(" ")[1],b[v]) for (a,b) in measures_dict.items()])
+        g.add_node(v,v_measures)
 
 def main():
-    # Load network data and remove header
-    dep_net=nx.read_edgelist("data/graph.csv",delimiter=",",create_using=nx.DiGraph(),nodetype=str)
-    dep_net.remove_edge("Package","Dependency") # Remove header, not an edge
-    main_component=nx.connected_component_subgraphs(dep_net.to_undirected())[0].nodes()
-    dep_net=nx.subgraph(dep_net,main_component)
+    """
+    Pre-processing: 
+        load data, compute centrality measures, write files with node data
+    """
+    # Load network data, create storage dict, and extract main component
+    depends=nx.read_edgelist("data/depends.csv",delimiter=",",create_using=nx.DiGraph(),nodetype=str)
+    suggests=nx.read_edgelist("data/suggests.csv",delimiter=",",create_using=nx.DiGraph(),nodetype=str)
+    imports=nx.read_edgelist("data/imports.csv",delimiter=",",create_using=nx.DiGraph(),nodetype=str)
+    nets_dict={"depends":depends,"suggests":suggests,"imports":imports}
+    for k in nets_dict.keys():
+        main_component=nx.connected_component_subgraphs(nets_dict[k].to_undirected())[0].nodes()
+        nets_dict[k]=nx.subgraph(nets_dict[k],main_component)
     
-    in_degree=dep_net.in_degree()
+    # Run multiple measures on graphs
+    measure_list=[nx.in_degree_centrality,nx.betweenness_centrality,nx.eigenvector_centrality,nx.pagerank]
+    for g in nets_dict.values():
+        multiple_measures(g,measure_list)
+        
+    # Output networks in GraphML format (to store node attributes)
+    for i in nets_dict.items():
+        nx.write_graphml(i[1],"data/"+i[0]+"_data.graphml")
+    print("All files written in GraphML format with node data")
     
-    # Convert to AGraph and add attributes
-    dep_ag=nx.to_agraph(dep_net)
-    dep_ag.graph_attr['rankdir']='TB'
+    """Visualization:
+        Various attempts to visualize data....starting with 'imports' first, as it is the smallest graph
+    """
     
-
-    # Write dot file
-    dep_ag.draw("dep_tree.pdf",prog="dot")
     
 
 if __name__ == '__main__':
